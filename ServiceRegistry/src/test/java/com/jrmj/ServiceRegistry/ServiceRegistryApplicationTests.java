85729772b4b1f90439738f765879a70093a63ffb
bob.w.perez <bob.w.perez@gmail.com>
2021-10-08 14:33:24 -0400

    added service registry files from Spring Init

---
+++ ServiceRegistry/src/test/java/com/jrmj/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrmj.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
