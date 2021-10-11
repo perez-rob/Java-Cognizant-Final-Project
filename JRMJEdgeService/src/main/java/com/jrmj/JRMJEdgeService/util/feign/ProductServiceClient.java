aeaf801731423139f1274aa75f2c63f9fa342708
bob.w.perez <bob.w.perez@gmail.com>
2021-10-11 12:00:52 -0400

    added name to Feing annotation

--- JRMJEdgeService/src/main/java/com/jrmj/JRMJEdgeService/util/feign/ProductServiceClient.java
+++ JRMJEdgeService/src/main/java/com/jrmj/JRMJEdgeService/util/feign/ProductServiceClient.java
@@ -3,7 +3,7 @@ package com.jrmj.JRMJEdgeService.util.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient
+@FeignClient(name = "product-service")
 public interface ProductServiceClient {
 
     @GetMapping("/products")
