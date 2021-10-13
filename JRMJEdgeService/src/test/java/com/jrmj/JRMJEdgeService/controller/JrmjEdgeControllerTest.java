f53ff15ad1d8d974e8470b90e15d07f0c8901346
bob.w.perez <bob.w.perez@gmail.com>
2021-10-13 11:02:49 -0400

    ned to finish Edge tests after researching how

--- JRMJEdgeService/src/test/java/com/jrmj/JRMJEdgeService/controller/JrmjEdgeControllerTest.java
+++ JRMJEdgeService/src/test/java/com/jrmj/JRMJEdgeService/controller/JrmjEdgeControllerTest.java
@@ -1,12 +1,17 @@
 package com.jrmj.JRMJEdgeService.controller;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(JrmjEdgeController.class)
@@ -15,6 +20,14 @@ public class JrmjEdgeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-
-
+//    @Test
+//    public void shouldReturnStatus201OnCustomerPostRequest() throws Exception {
+//        String inputJson = "{'email':'test@customer.net','password':'12345678','firstName':'Test','lastName':'Customer','addressShipping':{'id':null,'street1':'33 Happy Drive','street2':null,'city':'Atlanta','stateAbv':'GA','zipCode':'30350'},'addressBilling':{'id':null,'street1':'255 Test Street','street2':'Apt 5B','city':'Dunwoody','stateAbv':'GA','zipCode':'30333'},'phoneNumber':'404-444-4040'}";
+//
+//        mockMvc.perform(
+//                        post("/customers")
+//                                .content(inputJson)
+//                                .contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isCreated());
+//    }
 }
