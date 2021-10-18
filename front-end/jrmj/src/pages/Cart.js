c0663b44d03db11d3d9daae12ad41245c9855897
Jacob Beeson <beeson.jp@gmail.com>
2021-10-18 02:07:28 -0500

    cart page populates. next step: button functionality, stripe functionality.

--- front-end/jrmj/src/pages/Cart.js
+++ front-end/jrmj/src/pages/Cart.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import api from '../api';
 import Stripe from "react-stripe-checkout";
 import { Header, Footer } from "../components";
-import axios from 'axios';
-
+import { CartContext } from "../context/cart-context";
+import CartItem from '../components/CartItem';
 
 
 
 function Cart() {
-  
+  const { cartItems, itemCount, total, } = useContext(CartContext);
     return (
         <>
           <Header />
           <div className="content">
           <h1 className="text-center">Cart</h1>
-          <h4 className="text-center">(This is where items will populate)</h4>
+          { 
+          cartItems.length === 0 ? <div className="text-center"> Your cart is empty</div>
+          :
+           <>
+           <div className="cart-page">
+           <div className="multi-card-wrapper">
+             {
+               cartItems.map(item => <CartItem { ...item } key={item.shoeId} />)
+             }
+           </div>
+           </div>
+           </>
+           }
           <div className="text-center">
             <p>Total: $__</p>
             <button className="checkout-button">CHECKOUT</button>
