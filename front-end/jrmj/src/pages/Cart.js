3c52fc22b757c9facf7e7cbe034275d5f9c78eb4
Jacob Beeson <beeson.jp@gmail.com>
2021-10-18 08:48:32 -0500

    cart button functionality complete. all that is left is linking stripe to the checkout button

--- front-end/jrmj/src/pages/Cart.js
+++ front-end/jrmj/src/pages/Cart.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
-import api from '../api';
 import Stripe from "react-stripe-checkout";
 import { Header, Footer } from "../components";
 import { CartContext } from "../context/cart-context";
 import CartItem from '../components/CartItem';
 import axios from 'axios';
+import Total from '../context/Total';
 
 async function handleToken(token) {
   //console.log(token);
@@ -24,39 +24,38 @@ async function handleToken(token) {
 
 
 function Cart() {
-  const { cartItems, itemCount, total, } = useContext(CartContext);
-    return (
-        <>
-          <Header />
-          <div className="content">
-          <h1 className="text-center">Cart</h1>
-          { 
+  const { cartItems, itemCount, total, increase, decrease, removeProduct, clearCart } = useContext(CartContext);
+  const funcs = { increase, decrease, removeProduct }
+  return (
+    <>
+      <Header />
+      <div className="content">
+        <h1 className="text-center">Cart</h1>
+        {
           cartItems.length === 0 ? <div className="text-center"> Your cart is empty</div>
-          :
-           <>
-           <div className="cart-page">
-           <div className="multi-card-wrapper">
-             {
-               cartItems.map(item => <CartItem { ...item } key={item.shoeId} />)
-             }
-           </div>
-           </div>
-           </>
-           }
-          <div className="text-center">
-            <p>Total: $__</p>
-            <button className="checkout-button">CHECKOUT</button>
-            {/* <StripeCheckout /> */}
-            <button className="clear-button">CLEAR</button>
-            <div> <Stripe
+            :
+            <>
+              <div className="cart-page">
+                <div className="multi-card-wrapper">
+                  {
+                    cartItems.map(item => <CartItem {...item} key={item.shoeId} {...funcs } />)
+                  }
+                </div>
+              </div>
+              <Total itemCount={itemCount} total={total} clearCart={clearCart} />
+            </>
+        }
+        <div className="text-center">
+          {/* <StripeCheckout /> */}
+          <div> <Stripe
             stripeKey="pk_test_51JjhRnLiea7p0n0HUnks0w0ZwHFJC3jHH6wKqSw13QHcwO5QRvi7bQoB7FZIeWdkBI3qUoqC0vtHe8wAXI4Yq5BG00ZLAjHxJe"
             token={handleToken}
           /></div>
-          </div> 
-          <Footer />
-          </div>  
-        </>
-    )
+        </div>
+        <Footer />
+      </div>
+    </>
+  )
 
 }
 
